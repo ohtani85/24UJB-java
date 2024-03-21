27b9e305de64917e8997f45d27135a342debc0ed
21-manic-008g <21-manic-008g@21-manic-008>
2024-03-21 15:19:25 +0900

    OperatorExample

--- java2024/src/d0321/Operator4Example.java
+++ java2024/src/d0321/Operator4Example.java
@@ -7,12 +7,16 @@ public class Operator4Example {
 		int result1 = ++x + 10;
         int result2 = y++ + 10;
         
-        System.out.println(x);
-        System.out.println(y);
-        System.out.println(result1);
-        System.out.println(result2);
-
-
+        System.out.println("x : " + x);
+        System.out.println("y : " + y);
+        
+        int z = ++x + y++;
+        
+        System.out.println("x : " + x);
+        System.out.println("y : " + y);
+        System.out.println("z : " + z);
+        System.out.println("result1 : " + result1);
+        System.out.println("result2 : " + result2);
 	}
 
 }
---
+++ java2024/src/d0321/Operator5Example.java
@@ -0,0 +1,18 @@
+package d0321;
+
+public class Operator5Example {
+	public static void main(String[] args) {
+		boolean bPlay = true;
+		
+		System.out.println(bPlay);
+		
+		bPlay = !bPlay;
+		System.out.println(bPlay);
+		
+		bPlay = !bPlay;
+		System.out.println(bPlay);
+
+
+	}
+
+}
