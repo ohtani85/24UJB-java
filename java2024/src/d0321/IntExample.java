18db0659843e7b31f51d9f9933867437a88bcd6e
21-manic-008g <21-manic-008g@21-manic-008>
2024-03-21 11:59:33 +0900

    4th Commit

--- java2024/src/d0321/IntExample.java
+++ java2024/src/d0321/IntExample.java
@@ -9,7 +9,9 @@ public class IntExample {
 		System.out.println(var1);
 		System.out.println(var2);
 		System.out.println(var3);
-
+		
+		System.out.printf("8진수 : 0%o\n", var2);
+		System.out.printf("16진수 : 0x%x\n", var3);
 	}
 
 }
