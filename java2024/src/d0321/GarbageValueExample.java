a7cb65209ff71722f0acde9e1209a47709b9ed00
21-manic-008g <21-manic-008g@21-manic-008>
2024-03-21 11:47:34 +0900

    3rd Commit

--- java2024/src/d0321/GarbageValueExample.java
+++ java2024/src/d0321/GarbageValueExample.java
@@ -5,11 +5,11 @@ public class GarbageValueExample {
 		byte var1 = 125;
 		byte var2 = 125;
 
-		for(int i=0;i<5;i++) {
+		for (int i = 0; i < 5; i++) {
 			var1++;
 			var2++;
-			System.out.println("var1: " + var1 + "\t" + "var2:" + var2);
-		
+			System.out.println("var1: " + var1 + "\t" + "var2: " + var2);
+
 		}
 	}
 
