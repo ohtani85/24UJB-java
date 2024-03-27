7c376818389afbb195376802d48097f068431eb7
21-manic-008g <21-manic-008g@21-manic-008>
2024-03-27 15:20:21 +0900

    d0327

---
+++ java2024/src/d0327/Calculator55.java
@@ -0,0 +1,15 @@
+package d0327;
+
+public class Calculator55 {
+
+	    static double pi = 3.14159;
+	    
+	    static int plus(int x, int y) {
+	        return x + y;
+	    }
+	    
+	    static int minus(int x, int y) {
+	        return x - y;
+	        
+	    }
+	}
